398864186d65369abcf6f553845223cfafd033c7
kito saheki <sahechan0118@gmail.com>
2020-10-30 14:20:06 +0000

    mynews commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,6 +16,26 @@ class ProfileController extends Controller
     public function create()
     {
         return redirect('admin/profile/create');
+        {
+            $this->validate($request, Profile::$rules);
+            $profile = Profile::find($request->id);
+            $profile_form = $request->all();
+            if ($request->remove == 'true') {
+            $profile_form['image_path'] = null;
+            } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $profile_form['image_path'] = basename($path);
+            } else {
+            $profile_form['image_path'] = $profile->image_path;
+            }
+
+            unset($profile_form['image']);
+            unset($profile_form['remove']);
+            unset($profile_form['_token']);
+            // 該当するデータを上書きして保存する
+            $profile->fill($profile_form)->save();
+            return redirect('admin/profile/create');
+        }
     }
     
     public function edit()
@@ -27,4 +47,6 @@ class ProfileController extends Controller
     {
         return redirect('admin/profile/edit');
     }
+    
 }
+
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,41 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>profile edit</title>
-    </head>
-    <body>
-        <h1>プロフィール作成</h1>
-    </body>
-</html>
+@extend('layouts.admin')
+@section('title','プロフィールの編集')
+
+@section('content')
+    <div class='container'>
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールの編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach(4errors->aal() as $e)
+                                <li>{{ $e }</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名</label>
+                        <div class=""col-md-10>
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">性別</label>
+                        <div class=""col-md-10>
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">趣味</label>
+                        <div class=""col-md-10>
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">自己紹介欄</label>
+                        <div class=""col-md-10>
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                        </div>
+                    </div>
+                    
